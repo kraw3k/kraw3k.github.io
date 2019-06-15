19d03c80a041182108fe0d8ea0c90dbfef51f8a9
kraw3k <kacper5047@gmail.com>
2019-06-15 10:56:49 +0200

    Updates

---
+++ precache-manifest.6d250ee0f1f94f9b60c052c61f8a8133.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5360a91988b3dcf1403b7ab7ef02ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7423c6fdb7b4c1ad38c4",
+    "url": "/static/js/2.702fa99f.chunk.js"
+  },
+  {
+    "revision": "8b8a3cf1a7eb54a3f1fc",
+    "url": "/static/js/main.b032f6d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d37a870d5625c156a110ba0271a461cc",
+    "url": "/static/media/img_github.d37a870d.png"
+  },
+  {
+    "revision": "b495080cad40ddb4c45d50c21ef006b9",
+    "url": "/static/media/img_gmail.b495080c.png"
+  },
+  {
+    "revision": "dc76725372b4804ede9858c6c3625879",
+    "url": "/static/media/photo_about_1.dc767253.jpg"
+  },
+  {
+    "revision": "ce0fcaa2bc8004a92c947825b74a4e14",
+    "url": "/static/media/photo_about_2.ce0fcaa2.jpg"
+  },
+  {
+    "revision": "d84826173bad004331b2f7a370304373",
+    "url": "/static/media/photo_contact_1.d8482617.jpg"
+  },
+  {
+    "revision": "8efeeaa3bc196af9ca18501c8d47e38b",
+    "url": "/static/media/photo_contact_2.8efeeaa3.jpg"
+  },
+  {
+    "revision": "d294897dd5ea624f4658d877ea9a4b0a",
+    "url": "/static/media/photo_skills_1.d294897d.jpg"
+  },
+  {
+    "revision": "74d97ecb0ca700252934c40dbab7f238",
+    "url": "/static/media/photo_skills_2.74d97ecb.jpg"
+  }
+]);
